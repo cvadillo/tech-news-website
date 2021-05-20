b2a471de7a758622c6588f383b855a267db3ef7a
Carlos V <cavarod@gmail.com>
2021-05-20 16:17:42 -0700

    berkeley fakeout

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,9 @@
 // import the Sequelize constructor from the library
-const Sequelize = require('sequelize');
+const Sequelize = new Sequelize (process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+	host: 'localhost',
+	dialect: 'mysql',
+	port: 3306
+});
 
 require('dotenv').config();
 
